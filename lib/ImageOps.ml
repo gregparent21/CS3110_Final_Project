e2352f12f9cfb85a09f2df744fee9dcb76a81e1c
Evan Holly <egh67@cornell.edu>
2025-11-17 00:12:08 -0500

    Implement cut
    - modify ImageOps to include functions cut_square and cut_advanced
    - Added test cases for cut_square and cut_advanced
    - cut_advanced has faulty implementation

--- lib/ImageOps.ml
+++ lib/ImageOps.ml
@@ -1,8 +1,65 @@
 let screen_to_image_coords screen_x screen_y img_x img_y =
   (screen_x - img_x, screen_y - img_y)
 
-let is_within_bounds x y w h =
-  x >= 0 && x < w && y >= 0 && y < h
+let is_within_bounds x y w h = x >= 0 && x < w && y >= 0 && y < h
+let pixel_to_string r g b = Printf.sprintf "RGB(%d, %d, %d)" r g b
 
-let pixel_to_string r g b =
-  Printf.sprintf "RGB(%d, %d, %d)" r g b
+let cut_square data (start_x, start_y) (end_x, end_y) =
+  try
+    Array.mapi
+      (fun y row ->
+        Array.mapi
+          (fun x rgb ->
+            if start_x <= x && x <= end_x && start_y <= y && y <= end_y then
+              Graphics.rgb 255 255 255
+            else rgb)
+          row)
+      data
+  with _ -> raise (Failure "cut_advanced: invalid coordinates")
+
+(** [left (s_x, s_y) (e_x, e_y) (p_x, p_y)] determines if the point (p_x, p_y)
+    lives to the left or right of the directed line segment. *)
+let left (start_x, start_y) (end_x, end_y) (point_x, point_y) =
+  ((end_x - start_x) * (point_y - start_y))
+  - ((end_y - start_y) * (point_x - start_x))
+  > 0
+
+let on_segment (s_x, s_y) (e_x, e_y) (p_x, p_y) =
+  ((e_x - s_x) * (p_y - s_y)) - ((e_y - s_y) * (p_x - s_x)) = 0
+  && p_x >= min s_x e_x
+  && p_x <= max s_x e_x
+  && p_y >= min s_y e_y
+  && p_y <= max s_y e_y
+
+(** [winding_number (p_x, p_y) polygon] determines the winding number of the
+    point (p_x, p_y) with respect to the possible non-simple polygon. A zero
+    winding number means the point lies in the exterior and a non-zero winding
+    number means the point lies in the interior. *)
+let winding_number (p_x, p_y) polygon =
+  let n = List.length polygon in
+  let wn = ref 0 in
+  for i = 0 to n - 1 do
+    let x0, y0 = List.nth polygon i in
+    let x1, y1 = List.nth polygon ((i + 1) mod n) in
+    if on_segment (x0, y0) (x1, y1) (p_x, p_y) then wn := 1
+    else if y0 <= p_y then
+      if y1 > p_y && left (x0, y0) (x1, y1) (p_x, p_y) then wn := !wn + 1
+      else if y1 <= p_y && not (left (x0, y0) (x1, y1) (p_x, p_y)) then
+        wn := !wn - 1
+  done;
+  !wn
+
+(* Currently uses an inefficient algorithm (checks every possible pixel) that
+   handles even complex polygons. The algorithm is much simpler if we care about
+   only simple polygons. *)
+let cut_advanced data pairs =
+  try
+    Array.mapi
+      (fun y row ->
+        Array.mapi
+          (fun x rgb ->
+            if winding_number (x, y) pairs <> 0 then Graphics.rgb 255 255 255
+            else rgb)
+          row)
+      data
+  with _ -> raise (Failure "cut_advanced: invalid coordinates")
---
+++ test/test_FinalProject.ml
@@ -0,0 +1,54 @@
+open OUnit2
+open FinalProject
+open ImageOps
+
+let white = Graphics.rgb 255 255 255
+
+let tests =
+  "FinalProject Tests"
+  >::: [
+         ( "Test cut_square" >:: fun _ ->
+           let result =
+             cut_square [| [| 0; 12 |]; [| 42; 3110 |] |] (0, 0) (1, 1)
+           in
+           let expected = [| [| white; white |]; [| white; white |] |] in
+           assert_equal expected result;
+           let result =
+             cut_square
+               [| [| 0; 12; 34 |]; [| 42; 3110; 56 |]; [| 78; 90; 100 |] |]
+               (1, 1) (2, 2)
+           in
+           let expected =
+             [|
+               [| 0; 12; 34 |]; [| 42; white; white |]; [| 78; white; white |];
+             |]
+           in
+           assert_equal expected result;
+           let result = cut_square [| [| 1 |] |] (0, 0) (0, 0) in
+           let expected = [| [| white |] |] in
+           assert_equal expected result;
+           let result =
+             cut_square (Array.make_matrix 1000 1000 0) (500, 500) (600, 600)
+           in
+           let expected =
+             Array.init 1000 (fun y ->
+                 Array.init 1000 (fun x ->
+                     if 500 <= x && x <= 600 && 500 <= y && y <= 600 then white
+                     else 0))
+           in
+           assert_equal expected result );
+         ( "Test cut_advanced" >:: fun _ ->
+           let result =
+             cut_advanced
+               [| [| 0; 12; 34 |]; [| 42; 3110; 56 |]; [| 78; 90; 100 |] |]
+               [ (1, 1); (2, 2) ]
+           in
+           let expected =
+             [|
+               [| 0; 12; 34 |]; [| 42; white; white |]; [| 78; white; white |];
+             |]
+           in
+           assert_equal expected result );
+       ]
+
+let _ = run_test_tt_main tests
