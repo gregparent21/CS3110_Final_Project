9b688c10fb0be79b5bed51bcb1fcf54fcdf07680
Gregory Parent <gmp89@cornell.edu>
2025-12-03 15:38:07 -0500

    Add test cases for different file types loading

--- lib/ImageLoader.ml
+++ lib/ImageLoader.ml
@@ -11,41 +11,52 @@ let cmyk_pixel_to_rgb (px : Color.cmyk) : Color.rgb =
   let conv comp ink = int_of_float (255.0 *. (1.0 -. comp) *. (1.0 -. ink)) in
   { Color.r = conv c k; Color.g = conv m k; Color.b = conv y k }
 
-
-let graphics_image_of_file (path : string) : Graphics.image * int * int =
+let load_image_raw (path : string) : Images.t =
   Printf.eprintf "Loading image from: %s\n%!" path;
-  let raw = Images.load path [] in
+  Images.load path []
 
+let image_dimensions (img : Images.t) : int * int =
+  match img with
+  | Rgb24 i -> (i.Rgb24.width, i.Rgb24.height)
+  | Rgba32 i ->
+      let rgb = Rgb24.of_rgba32 i in
+      (rgb.Rgb24.width, rgb.Rgb24.height)
+  | Index8 i ->
+      let rgb = Index8.to_rgb24 i in
+      (rgb.Rgb24.width, rgb.Rgb24.height)
+  | Index16 i ->
+      let rgb = Index16.to_rgb24 i in
+      (rgb.Rgb24.width, rgb.Rgb24.height)
+  | Cmyk32 i -> (i.Cmyk32.width, i.Cmyk32.height)
+
+let make_graphics_image (img : Images.t) : Graphics.image =
   let rgb =
-    match raw with
-    | Images.Rgb24 img ->
-        Printf.printf "Detected format: Rgb24\n%!";
-        img
-    | Images.Index8 img ->
-        Printf.printf "Detected format: Index8 -> converting to Rgb24\n%!";
-        Index8.to_rgb24 img
-    | Images.Index16 img ->
-        Printf.printf "Detected format: Index16 -> converting to Rgb24\n%!";
-        Index16.to_rgb24 img
-    | Images.Rgba32 img ->
-        Printf.printf "Detected format: Rgba32 -> converting to Rgb24\n%!";
-        Rgb24.of_rgba32 img
-    | Images.Cmyk32 img ->
-        Printf.printf "Detected format: Cmyk32 -> Rgb24 (manual)\n%!";
-        let w = img.Cmyk32.width and h = img.Cmyk32.height in
+    match img with
+    | Rgb24 i -> i
+    | Rgba32 i -> Rgb24.of_rgba32 i
+    | Index8 i -> Index8.to_rgb24 i
+    | Index16 i -> Index16.to_rgb24 i
+    | Cmyk32 i ->
+        let w = i.Cmyk32.width and h = i.Cmyk32.height in
         let out = Rgb24.create w h in
         for yy = 0 to h - 1 do
           for xx = 0 to w - 1 do
-            let px = Cmyk32.get img xx yy in
-            Rgb24.set out xx yy (cmyk_pixel_to_rgb px)
+            let px = Cmyk32.get i xx yy in
+            Rgb24.set out xx yy
+              (let c = float_of_int px.Color.c /. 255.0
+               and m = float_of_int px.Color.m /. 255.0
+               and y = float_of_int px.Color.y /. 255.0
+               and k = float_of_int px.Color.k /. 255.0 in
+               let conv comp ink =
+                 int_of_float (255.0 *. (1.0 -. comp) *. (1.0 -. ink))
+               in
+               { Color.r = conv c k; Color.g = conv m k; Color.b = conv y k })
           done
         done;
         out
   in
-
   let w = rgb.Rgb24.width in
   let h = rgb.Rgb24.height in
-
   let data = Array.make_matrix h w 0 in
   for y = 0 to h - 1 do
     for x = 0 to w - 1 do
@@ -53,19 +64,9 @@ let graphics_image_of_file (path : string) : Graphics.image * int * int =
       data.(y).(x) <- (r lsl 16) lor (g lsl 8) lor b
     done
   done;
+  Graphics.make_image data
 
-  (Graphics.make_image data, w, h)
-
-(* Inline tests for CMYK -> RGB conversion logic *)
-
-let%test "cmyk_white_to_rgb_white" =
-  let rgb = cmyk_pixel_to_rgb { Color.c = 0; m = 0; y = 0; k = 0 } in
-  rgb.Color.r = 255 && rgb.Color.g = 255 && rgb.Color.b = 255
-
-let%test "cmyk_black_to_rgb_black" =
-  let rgb = cmyk_pixel_to_rgb { Color.c = 0; m = 0; y = 0; k = 255 } in
-  rgb.Color.r = 0 && rgb.Color.g = 0 && rgb.Color.b = 0
-
-let%test "cmyk_cyan_to_rgb" =
-  let rgb = cmyk_pixel_to_rgb { Color.c = 255; m = 0; y = 0; k = 0 } in
-  rgb.Color.r = 0 && rgb.Color.g = 255 && rgb.Color.b = 255
+let graphics_image_of_file (path : string) : Graphics.image * int * int =
+  let img = load_image_raw path in
+  let w, h = image_dimensions img in
+  (make_graphics_image img, w, h)
